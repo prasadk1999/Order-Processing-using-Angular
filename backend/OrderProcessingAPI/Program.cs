4d57249cd2e5674dbd46fa08fae9b4aa10019b41
prasadk1999 <prasad.m.kukade@gmail.com>
2024-11-21 14:12:16 +0530

    first commit

---
+++ backend/OrderProcessingAPI/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using OrderProcessingAPI.DatabaseContext;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrderProcessingContext>(options => 
+    options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
